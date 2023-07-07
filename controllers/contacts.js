104bc4ac263288499cdea98bf822c9f356e534e4
SerhiiRud <carmagsales@gmail.com>
2023-07-07 22:34:57 +0300

    update

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,47 +1,49 @@
-const contacts = require("../models/contacts");
+const { Contact } = require("../models/contact");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (__, res) => {
-  const result = await contacts.listContacts();
+  const result = await Contact.find();
   res.status(200).json(result);
 };
 
 const getById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.getContactById(contactId);
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
   if (!result) {
-      throw HttpError(404, "Not Found");
+    throw HttpError(404, "Not Found");
   }
   res.status(200).json(result);
 };
 
 const add = async (req, res) => {
-  const result = await contacts.addContact(req.body);
+  const result = await Contact.create(req.body);
   res.status(201).json(result);
 };
 
-const deleteById = async (req, res) => {
-  const { contactId } = req.params;
-  const result = await contacts.removeContact(contactId);
-  if (!result) {
-    throw HttpError(404, "Not Found");
-  }
-  res.status(200).json({ message: "contact deleted" });
-};
+// const deleteById = async (req, res) => {
+//   const { contactId } = req.params;
+//   const result = await contacts.removeContact(contactId);
+//   if (!result) {
+//     throw HttpError(404, "Not Found");
+//   }
+//   res.status(200).json({ message: "contact deleted" });
+// };
 
-const updateById = async (req, res) => {
-  const { contactId } = req.params;
-  const result = await contacts.updateContact(contactId, req.body);
-  if (!result) {
-    throw HttpError(404, "Not Found");
-  }
-  res.status(200).json(result);
-};
+// const updateById = async (req, res) => {
+//   const { contactId } = req.params;
+//   const result = await contacts.updateContact(contactId, req.body);
+//   if (!result) {
+//     throw HttpError(404, "Not Found");
+//   }
+//   res.status(200).json(result);
+// };
 
 module.exports = {
   getAll: ctrlWrapper(getAll),
   getById: ctrlWrapper(getById),
   add: ctrlWrapper(add),
-  deleteById: ctrlWrapper(deleteById),
-  updateById: ctrlWrapper(updateById),
+  // deleteById: ctrlWrapper(deleteById),
+  // updateById: ctrlWrapper(updateById),
 };
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
-const { DB_HOST, PORT } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
