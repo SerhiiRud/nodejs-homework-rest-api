890226cf6eca9299298ad1adff8b7f94765c3218
SerhiiRud <carmagsales@gmail.com>
2023-07-07 17:55:46 +0300

    env

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const app = require("./app");
-const { DB_HOST } = require("./config");
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
+const { DB_HOST } = process.env;
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
@@ -13,7 +14,3 @@ mongoose
     console.log(error.message);
     process.exit(1);
   });
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
