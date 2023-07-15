7841c4f98e42adb5086b894a32ece09acc019a3f
SerhiiRud <carmagsales@gmail.com>
2023-07-15 15:38:51 +0300

    update

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,7 +2,7 @@ const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page, limit, favorite } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
   const result = await Contact.find(
     favorite ? { owner, favorite } : { owner },
