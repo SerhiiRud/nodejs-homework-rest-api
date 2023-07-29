a19f5ce89444fbf5270c6b03e50bbf69a31bb30c
SerhiiRud <carmagsales@gmail.com>
2023-07-29 18:01:11 +0300

    update

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -4,6 +4,7 @@ const getCurrent = require("./getCurrent");
 const logout = require("./logout");
 const updateSubscription = require("./updateSubscription");
 const updateAvatar = require("./updateAvatar");
+
 const { ctrlWrapper } = require("../../helpers");
 
 module.exports = {
